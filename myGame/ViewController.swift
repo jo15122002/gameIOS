97ec56f137138f0742907cdeb1ba7043ade69d94
Digital <digital@BASS-C17.local>
2022-11-28 16:34:10 +0100

    ajout du rebondissement non fonctionnel

--- myGame/ViewController.swift
+++ myGame/ViewController.swift
@@ -11,12 +11,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerTwo: UIView!
     @IBOutlet weak var playerOne: UIView!
     @IBOutlet weak var ball: UIView!
+    @IBOutlet weak var rightWall: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Timer.scheduledTimer(withTimeInterval: 1.0/20.0, repeats: true) { t in
+            
+            var dirX = 1.0
+            var dirY = -1.0
             //Code executé 20x/sec
+            self.ball.center.x += dirX
+            self.ball.center.y += dirY
+        
+            if(self.playerTwo.frame.contains(self.ball.center)){
+                dirY = -1.0
+            }
+            
+            if(self.rightWall.frame.contains(self.ball.center)){
+                dirX = -1.0
+            }
         }
     }
     @IBAction func rightClicked(_ sender: Any) {
