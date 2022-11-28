fc3db59f859b856293ea163210af2609cd481b0f
Digital <digital@BASS-C17.local>
2022-11-28 16:22:11 +0100

    ajout d'un framerate

--- myGame/ViewController.swift
+++ myGame/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var playerTwo: UIView!
+    @IBOutlet weak var playerOne: UIView!
+    @IBOutlet weak var ball: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(withTimeInterval: 1.0/20.0, repeats: true) { t in
+            //Code executé 20x/sec
+        }
     }
-
+    @IBAction func rightClicked(_ sender: Any) {
+        self.playerTwo.center.x += 1
+    }
+    @IBAction func leftClicked(_ sender: Any) {
+        self.playerTwo.center.x += -1
+    }
+    
 
 }
 
